675e8337370d19bdc6bccc4eec63028f18290bb2
AkankshRakesh <akankshrakesh@gmail.com>
2024-11-02 21:59:04 +0530

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+# ML Trascription & Translation Tool
+
+- built a transcription and translation web application that runs 2 machine learning models in the users browser for free with React.js, Huggingface.js & TailwindCSS. </br>
+DEMO: https://ml-free-scribe.vercel.app/
+
+
+![Capture d’écran 2023-10-20 151100](https://github.com/raamlaa/free-scribe/assets/94558213/91f6f569-2c7c-48c0-9770-5aaac770dac6)
+
+
+
+
+
+
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from "../assets/logo.png"
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between gap-4 p-4">
+          <div className="flex items-center gap-2">
+          <img className="w-10" src={logo}/>
+          <a href="/"><h1 className="font-semibold">Audio<span className="text-blue-400 bold ">Bridge</span></h1></a>
+          </div>
+          <a href="/" className='flex items-center gap-2 specialBtn px-3 py-2 rounded-lg text-blue-400'>
+                    <p>New</p>
+                    <i className="fa-solid fa-plus"></i>
+                </a>
+        </header>
+  )
+}
